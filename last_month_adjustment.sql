801ed5daa5f083373dcb9fe740c1dcc81d50d1b3
sidstefani <118837391+sidstefani@users.noreply.github.com>
2023-08-25 07:55:12 -0700

    Update last_month_adjustment.sql

--- last_month_adjustment.sql
+++ last_month_adjustment.sql
@@ -1,5 +1,6 @@
 SELECT
     gcp_billing_export.adjustment_info.description  AS gcp_billing_export_adjustment_info__description,
+    --- Consider changing the case statement to regular format 
     CASE
         WHEN gcp_billing_export.adjustment_info.type = 'USAGE_CORRECTION' THEN 'Usage Correction'
         WHEN gcp_billing_export.adjustment_info.type = 'GENERAL_ADJUSTMENT' THEN 'General Adjustment'
@@ -14,7 +15,7 @@ SELECT
     END  AS gcp_billing_export_adjustment_info__mode,
     gcp_billing_export.adjustment_info.id  AS gcp_billing_export_adjustment_info__id
 FROM `INSERT ALIAS HERE` AS gcp_billing_export
-//// Change from the month of 4 to the last complete month 
+---- Change from the month of 4 to the last complete month 
 WHERE ((( date(CAST(substring(gcp_billing_export.invoice.month,1,4) AS int),CAST(substring(gcp_billing_export.invoice.month,5,2) AS int),01) ) >= ((DATE('2022-04-01'))) AND ( date(CAST(substring(gcp_billing_export.invoice.month,1,4) AS int),CAST(substring(gcp_billing_export.invoice.month,5,2) AS int),01) ) < ((DATE_ADD(DATE('2022-04-01'), INTERVAL 1 MONTH)))))
 GROUP BY
     1,
