e34323b441154e832e38fec0e535ee8f34825e40
sidstefani <118837391+sidstefani@users.noreply.github.com>
2023-08-25 07:59:05 -0700

    Create cost_by_org_id.sql

---
+++ cost_by_org_id.sql
@@ -0,0 +1,12 @@
+SELECT
+    REGEXP_EXTRACT( gcp_billing_export.project.ancestry_numbers,"^/([0-9]+)")  AS gcp_billing_export_gcp_org_id,
+    COALESCE(SUM(gcp_billing_export.cost ), 0) AS gcp_billing_export_total_cost
+FROM `sidney-stefani.looker_scratch.LR_CMQSD1681753303785_gcp_billing_export` AS gcp_billing_export
+WHERE ((( gcp_billing_export.usage_start_date
+     ) >= ((DATE_ADD(DATE_TRUNC(CURRENT_DATE('America/Los_Angeles'), MONTH), INTERVAL -5 MONTH))) AND ( gcp_billing_export.usage_start_date
+     ) < ((DATE_ADD(DATE_ADD(DATE_TRUNC(CURRENT_DATE('America/Los_Angeles'), MONTH), INTERVAL -5 MONTH), INTERVAL 6 MONTH)))))
+GROUP BY
+    1
+ORDER BY
+    2 DESC
+LIMIT 500
