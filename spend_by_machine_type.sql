e776c201e71a6eded7780de8ff3b4d47cc770ad8
sidstefani <118837391+sidstefani@users.noreply.github.com>
2023-08-31 11:28:49 -0700

    Update spend_by_machine_type.sql

--- spend_by_machine_type.sql
+++ spend_by_machine_type.sql
@@ -9,28 +9,12 @@ SELECT
           when gcp_billing_export.sku.description like "%Commitment v1: Cpu in%" then "N1"
           when gcp_billing_export.sku.description like "%Commitment v1: Ram in%" then "N1"
           else "Other" end AS gcp_billing_export_machine_type,
-    CASE
-          WHEN gcp_billing_export.currency = 'USD' THEN '$'
-          WHEN gcp_billing_export.currency = 'EUR' THEN '€'
-          WHEN gcp_billing_export.currency = 'JPY' THEN '¥'
-          WHEN gcp_billing_export.currency = 'AUD'THEN 'A$'
-          WHEN gcp_billing_export.currency = 'BRL'THEN 'R$'
-          WHEN gcp_billing_export.currency = 'CAD'THEN 'C$'
-          WHEN gcp_billing_export.currency = 'HKD' THEN 'HK$'
-          WHEN gcp_billing_export.currency = 'INR' THEN '₹'
-          WHEN gcp_billing_export.currency = 'IDR' THEN 'Rp'
-          WHEN gcp_billing_export.currency = 'ILS' THEN '₪'
-          WHEN gcp_billing_export.currency = 'MXN' THEN 'Mex$'
-          WHEN gcp_billing_export.currency = 'NZD' THEN 'NZ$'
-          WHEN gcp_billing_export.currency = 'GBP' THEN '£'
-          ELSE CONCAT(gcp_billing_export.currency, ' ')
-        END AS gcp_billing_export_currency_symbol,
-    COALESCE(SUM(gcp_billing_export.cost ), 0) AS gcp_billing_export_total_cost,
-        COALESCE(SUM(gcp_billing_export.cost ), 0) - COALESCE(SUM(( ( SELECT SUM(-gcp_billing_export__credits.amount) FROM UNNEST(gcp_billing_export.credits) as gcp_billing_export__credits  )  ) ), 0) AS gcp_billing_export_total_net_cost
-FROM `sidney-stefani.looker_scratch.LR_CMQSD1681753303785_gcp_billing_export` AS gcp_billing_export
+        ROUND(COALESCE(CAST( ( SUM(DISTINCT (CAST(ROUND(COALESCE( (gcp_billing_export.cost ) ,0)*(1/1000*1.0), 9) AS NUMERIC) + (cast(cast(concat('0x', substr(to_hex(md5(CAST( (gcp_billing_export.pk )  AS STRING))), 1, 15)) as int64) as numeric) * 4294967296 + cast(cast(concat('0x', substr(to_hex(md5(CAST( (gcp_billing_export.pk )  AS STRING))), 16, 8)) as int64) as numeric)) * 0.000000001 )) - SUM(DISTINCT (cast(cast(concat('0x', substr(to_hex(md5(CAST( (gcp_billing_export.pk )  AS STRING))), 1, 15)) as int64) as numeric) * 4294967296 + cast(cast(concat('0x', substr(to_hex(md5(CAST( (gcp_billing_export.pk )  AS STRING))), 16, 8)) as int64) as numeric)) * 0.000000001) )  / (1/1000*1.0) AS NUMERIC), 0), 6) - COALESCE(SUM(-1* gcp_billing_export__credits.amount   ), 0) AS gcp_billing_export_total_net_cost
+FROM `ENTER ALIAS HERE` AS gcp_billing_export
+LEFT JOIN UNNEST(gcp_billing_export.credits) as gcp_billing_export__credits
+WHERE (gcp_billing_export.service.description ) = 'Compute Engine'
 GROUP BY
-    1,
-    2
+    1
 ORDER BY
-    3 DESC
+    2 DESC
 LIMIT 500
